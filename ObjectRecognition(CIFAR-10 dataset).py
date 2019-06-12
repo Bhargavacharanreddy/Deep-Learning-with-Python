0ddc73cc9a0643b6afa9c372c85847a392aad884
Bhargava Charan <bhargavacharan1316@gmail.com>
2019-06-12 17:36:28 -0400

    Lesson13
    contains the code for loading the CIFAR-10 dataset for object recognition.

---
+++ ObjectRecognition(CIFAR-10 dataset).py
@@ -0,0 +1,12 @@
+from keras.datasets import cifar10
+from keras.utils import np_utils
+# load data
+(X_train, y_train), (X_test, y_test) = cifar10.load_data()
+# normalize inputs from 0-255 to 0.0-1.0
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train = X_train / 255.0
+X_test = X_test / 255.0
+# one hot encode outputs
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
